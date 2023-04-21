94fe23cb6d572c54d6d12ab62bb95383332039ae
LiAo <liao991224@gmail.com>
2023-04-21 13:57:47 +0800

    LiAo: epub 读取乱码

--- src/main/java/com/liao/book/parse/EpubContentParser.java
+++ src/main/java/com/liao/book/parse/EpubContentParser.java
@@ -1,14 +1,16 @@
 package com.liao.book.parse;
 
-import com.liao.book.utils.LocalCharsetUtil;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.zip.ZipEntry;
@@ -31,14 +33,20 @@ public class EpubContentParser {
      * @return <章节，章节内容>
      */
     public static Map<String, String> parseEpub(String file) {
+
+        // 存储小说
         Map<String, String> result = new LinkedHashMap<>();
-        try (ZipInputStream zipStream = new ZipInputStream(new FileInputStream(file), Charset.forName(LocalCharsetUtil.getFileCharset(file)))) {
+
+        // 存储文件名称对应的编码格式
+        Map<String, Charset> charsetMap = getCharsetMap(file);
+
+        try (ZipInputStream zipStream = new ZipInputStream(new FileInputStream(file))) {
             ZipEntry entry;
             while ((entry = zipStream.getNextEntry()) != null) {
                 if (!entry.isDirectory() && entry.getName().endsWith(".html") && entry.getName().contains("chapter")) {
 
-                    String content = readEntry(zipStream);
-                    Document doc = Jsoup.parse(content);
+                    Document doc = readEntry(zipStream, charsetMap.get(entry.getName()));
+
                     String title = getTitle(doc);
                     String chapterContent = getContent(doc);
 
@@ -59,9 +67,11 @@ public class EpubContentParser {
      * @param zipStream epub 单个文件
      * @return 内容
      */
-    private static String readEntry(ZipInputStream zipStream) {
+    private static Document readEntry(ZipInputStream zipStream, Charset c) {
         StringBuilder sb = new StringBuilder();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(zipStream));
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(zipStream, c));
+
         String line;
         while (true) {
             try {
@@ -72,7 +82,61 @@ public class EpubContentParser {
             sb.append(line);
         }
 
-        return sb.toString();
+        return Jsoup.parse(sb.toString());
+    }
+
+    /**
+     * 获取 EPUB 中每一个文档的编码格式
+     *
+     * @param filePath 文件路径
+     * @return Map<文件名称, 编码格式>
+     */
+    private static Map<String, Charset> getCharsetMap(String filePath) {
+        Map<String, Charset> charsetMap = new HashMap<>(16);
+
+        try (ZipInputStream zipStream = new ZipInputStream(new FileInputStream(filePath))) {
+            ZipEntry entry;
+            while ((entry = zipStream.getNextEntry()) != null) {
+                if (!entry.isDirectory() && entry.getName().endsWith(".html") && entry.getName().contains("chapter")) {
+
+                    Charset charset = getCharset(zipStream);
+
+                    charsetMap.put(entry.getName(), charset);
+                }
+                zipStream.closeEntry();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return charsetMap;
+    }
+
+    /**
+     * 获取文档编码格式
+     *
+     * @param zipStream zip
+     * @return 编码格式
+     */
+    private static Charset getCharset(ZipInputStream zipStream) {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(zipStream));
+        String line;
+        while (true) {
+            try {
+                if ((line = reader.readLine()) == null) break;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            sb.append(line);
+
+            if (line.contains("<?xml")) break;
+        }
+
+        Document parse = Jsoup.parse(sb.toString());
+
+        return parse.charset();
     }
 
     /**

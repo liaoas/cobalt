7a4e62d48e31c7f1f5ed4c95a7e2ce5d519524c0
LiAo <46950266+liaoas@users.noreply.github.com>
2024-07-18 12:37:09 +0800

    ModuleConstants Code Style

--- src/main/java/com/cobalt/common/constant/ModuleConstants.java
+++ src/main/java/com/cobalt/common/constant/ModuleConstants.java
@@ -14,7 +14,6 @@ public class ModuleConstants {
 
     // 表头内容
     public static String[] head = {"文章名称", "最新章节", "作者", "更新", "链接"};
-
     // 搜索结果表格
     public static DefaultTableModel tableModel = new DefaultTableModel(null, head);
 
@@ -22,13 +21,10 @@ public class ModuleConstants {
 
     // 笔趣阁
     public static final String BI_QU_GE = "笔趣阁$";
-
     // 笔趣阁
     public static final String XIANG_SHU = "香书小说";
-
     // 本地导入
     public static final String IMPORT = "导入";
-
     // 网络
     public static final String NETWORK = "网络";
 
@@ -36,27 +32,21 @@ public class ModuleConstants {
 
     // 选项卡标签
     public static final String TAB_CONTROL_TITLE_HOME = "首页";
-
     public static final String TAB_CONTROL_TITLE_UNFOLD = "全屏";
-
     public static final String TAB_CONTROL_READING_HISTORY = "帮助";
 
     /*****************组件悬浮提示*****************/
+
     // 搜索按钮
     public static final String SEARCH_BTN = "搜索";
-
     // 开始阅读
     public static final String START_READ = "开始阅读";
-
     // 上一章
     public static final String BTN_ON = "上一章";
-
     // 下一章
     public static final String UNDER_ON = "下一章";
-
     // 跳转章节
     public static final String JUMP_BUTTON = "章节跳转";
-
     // 文字设置
     public static final String SETTINGS = "设置";
 
